43dbb42789547d5b26f1e8ce6576c06d82ad7c87
ridwane992003-lgtm <ridwane992003@gmail.com>
2026-07-04 09:36:37 +0000

    fix: use output export in config and remove deprecated next export command

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 💡 C'est la méthode moderne pour générer le site statique (remplace next export)
   eslint: {
     ignoreDuringBuilds: true,
   },
