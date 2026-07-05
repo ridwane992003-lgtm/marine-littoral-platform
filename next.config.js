b45d7213d2aebe8aab35d955a5e24d448e47bafc
ridwane992003-lgtm <ridwane992003@gmail.com>
2026-07-05 15:59:42 +0000

    fix: restore clean layout and actions for vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig = {
   output: 'export',
-  trailingSlash: true,
-  // 💡 Indique à Next.js le nom de votre dépôt GitHub pour que tous les liens fonctionnent
-  basePath: '/marine-littoral-platform', 
+  // On n'active le trailingSlash et le basePath QUE pour GitHub Actions
+  trailingSlash: isGithubActions ? true : false,
+  basePath: isGithubActions ? '/marine-littoral-platform' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
