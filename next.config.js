a27774d46cf3c5305be1ab58266391bfea577912
ridwane992003-lgtm <ridwane992003@gmail.com>
2026-07-05 16:07:00 +0000

    chore: optimize exclusively for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
 const nextConfig = {
-  output: 'export',
-  // On n'active le trailingSlash et le basePath QUE pour GitHub Actions
-  trailingSlash: isGithubActions ? true : false,
-  basePath: isGithubActions ? '/marine-littoral-platform' : '',
+  // Vercel gère le build automatiquement, on a juste besoin d'ignorer ESLint pour éviter le crash
   eslint: {
     ignoreDuringBuilds: true,
   },
